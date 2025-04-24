91eafefb6d3a1c5f4fb46e8190d8e71fac140307
briannaordaz <briannaordaz5@gmail.com>
2025-04-24 13:06:02 -0700

    added author, course, faculty, and purpose

---
+++ COMP003.CodingAssignment5/Program.cs
@@ -0,0 +1,39 @@
+
+//Author: Brianna Ordaz
+//Course: COMP003B
+//Faculty Name: Jonathan Cruz
+//Purpose: Demonstrate CRUD operations via ASP.NET Core Web API
+namespace COMP003.CodingAssignment5;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
