15db499d0abb54aadfbcdf9198215969642f1729
briannaordaz <briannaordaz5@gmail.com>
2025-04-24 13:09:56 -0700

    added Ticket.cs to models and added 4 properties

---
+++ COMP003.CodingAssignment5/Models/Ticket.cs
@@ -0,0 +1,13 @@
+namespace COMP003.CodingAssignment5.Models;
+
+public class Ticket
+{
+    public int TicketId { get; set; }
+    
+    public string MovieName { get; set; } = string.Empty;
+    
+    public decimal TicketCost { get; set; }
+    
+    public DateTime StartDate { get; set; }
+    
+}
